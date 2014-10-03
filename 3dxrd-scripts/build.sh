b77c81d8d4fde9e13c9dd0f3ae93e7624f2af717
Darren Dale <dsdale24@gmail.com>
2014-10-03 11:01:52 -0400

    adds tweakdetpars and filter_images scripts

--- 3dxrd-scripts/build.sh
+++ 3dxrd-scripts/build.sh
@@ -2,8 +2,8 @@ mkdir -p $PREFIX/bin
 cp -r $RECIPE_DIR/findpeaks $PREFIX/bin/
 cp -r $RECIPE_DIR/makemap_all $PREFIX/bin/
 cp -r $RECIPE_DIR/grainspotter_loop $PREFIX/bin/
-cp -r $RECIPE_DIR/tweakpars_*.py $PREFIX/bin/
-cp -r $RECIPE_DIR/correct_*.py $PREFIX/bin/
+cp -r $RECIPE_DIR/tweakdetpars $PREFIX/bin/
+cp -r $RECIPE_DIR/filter_images $PREFIX/bin/
 cp -r $RECIPE_DIR/ge2tiff $PREFIX/bin/
 
 mkdir -p $PREFIX/share/3dxrd
