e22f0a09c2610a496420790b594ed93ec27be2fa
Darren Dale <dsdale24@gmail.com>
2014-10-08 15:07:36 -0400

    fixes tk linking issue on osx

--- pillow/build.sh
+++ pillow/build.sh
@@ -4,6 +4,7 @@ export TIFF_ROOT=$PREFIX
 export JPEG_ROOT=$PREFIX
 export ZLIB_ROOT=$PREFIX
 export FREETYPE2_ROOT=$PREFIX
+export CFLAGS="-I$PREFIX/include -L$PREFIX/lib"
 
 $PYTHON setup.py install
 
