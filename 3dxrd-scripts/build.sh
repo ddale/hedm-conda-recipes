df37042e0e1d75513fee2ffcad6d377cb451d844
Darren Dale <dsdale24@gmail.com>
2015-03-26 11:20:04 -0400

    adds stack_layers

--- 3dxrd-scripts/build.sh
+++ 3dxrd-scripts/build.sh
@@ -5,6 +5,7 @@ cp -r $RECIPE_DIR/grainspotter_loop $PREFIX/bin/
 cp -r $RECIPE_DIR/tweakdetpars $PREFIX/bin/
 cp -r $RECIPE_DIR/filter_images $PREFIX/bin/
 cp -r $RECIPE_DIR/ge2tiff $PREFIX/bin/
+cp -r $RECIPE_DIR/stack_layers $PREFIX/bin/
 
 mkdir -p $PREFIX/share/3dxrd
 cp $RECIPE_DIR/*.ini $PREFIX/share/3dxrd/
